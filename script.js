3e920ef948bea3f62cb7715c0ab19a525602701d
Croooook <rajatgoyal6404@gmail.com>
2022-02-22 17:53:32 +0530

    minor

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const axios = require("axios");
 const ethers = require("ethers");
 
 const dotenv = require("dotenv").config();
+import { request, gql } from 'graphql-request'
 
 const main = async () => {
   var thresh = ``;
   var result;
+  let response = []
   //var ctr=1;
   const dsa_accounts = [];
   let URL = "https://api.thegraph.com/subgraphs/name/croooook/dsa-accounts";
@@ -28,26 +30,28 @@ const main = async () => {
     if (Object.values(result.data.data.logAccountCreateds).length === 0) break;
     let datas = Object.values(result.data.data.logAccountCreateds);
     for (let data of datas) {
-      dsa_accounts.push(data.owner);
+      dsa_accounts.push(String(data.owner));
     }
     thresh =
       result.data.data.logAccountCreateds[
         Object.values(result.data.data.logAccountCreateds).length - 1
       ].id;
   }
-
+console.log(dsa_accounts.slice(0, 100))
   thresh = ``;
   URL =
-    "https://api.thegraph.com/subgraphs/id/QmW9Vn8uHdTr3nq99VbKgxVDC3siaHfmvR2MpkEsbF5wXV";
-
+    "https://api.thegraph.com/subgraphs/name/thrilok209/instadapp-uniswap-v3";
+  let i = 0;
+  let end = dsa_accounts.length
+  
   while (true) {
     const res = await axios.post(URL, {
       query:
         `
             {
-                positions(where: {id_gt:"` +
-        thresh +
-        `" owner_in: ${dsa_accounts}){
+                positions(first: 1000, where: {id_gt:"` +
+                thresh +
+                `"  owner_in : ${dsa_accounts.slice(i, i + 1000)}}){
                     id
                     owner
                     pool
@@ -55,9 +59,17 @@ const main = async () => {
             }
             `,
     });
-    if (Object.values(!res.data.data || res.data.data.positions.length) === 0)
-      break;
-    datas1 = Object.values(res.data.data.positions);
+
+    if(i > end) break
+    i += 1000;
+    const res1= await request (getGraphUrl(URL),query)
+    // console.log(res.data)
+    if (!res.data.data) continue;
+    if (!res.data.data.positions) continue;
+
+    datas1 = res.data.data.positions;
+    console.log("🚀 ~ file: script.js ~ line 62 ~ main ~ datas1", datas1)
+    response = response.concat(datas1)
     thresh =
       res.data.data.positions[Object.values(res.data.data.positions).length - 1]
         .id;
