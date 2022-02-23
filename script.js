082ab4e64f215f9605cb7b1278046930637cbf3e
Croooook <rajatgoyal6404@gmail.com>
2022-02-23 23:00:20 +0530

    changes made for liq to usd

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const ethers = require("ethers");
 
 const dotenv = require("dotenv").config();
 const { request, gql } = require("graphql-request");
-
+const BigNumber = require("bignumber.js");
 const alchemy_key = `https://polygon-mainnet.g.alchemy.com/v2/9VPZHrKVkuzR6gAqaUZi6SpOlBZobRcO`;
 
 let stakerabi = require("./staker-abi");
@@ -13,9 +13,9 @@ let staker_contract = new ethers.Contract(addr_staker, stakerabi, provider);
 
 let liqabi = require("./liquidity-abi.json");
 let addr_liq = "0x878C410028E3830f1Fe03C428FF95012111Ae1f1".toLowerCase();
-let liq_contract = new ethers.Contract(addr_liq , liqabi , provider);
+let liq_contract = new ethers.Contract(addr_liq, liqabi, provider);
 
-
+let poolabi = require("./pool-abi.json");
 const main = async () => {
   let nft_owner = await staker_contract.deposits("62542");
   console.log(nft_owner.owner);
@@ -51,9 +51,8 @@ const main = async () => {
         Object.values(result.data.data.logAccountCreateds).length - 1
       ].id;
   }
- 
-console.log(dsa_accounts.get("0x4dee144e4d60ad8ae3e4b53e09669349dc0e23da"));
-  
+
+  console.log(dsa_accounts.get("0x4dee144e4d60ad8ae3e4b53e09669349dc0e23da"));
 
   thresh = `0`;
   URL =
@@ -72,6 +71,8 @@ console.log(dsa_accounts.get("0x4dee144e4d60ad8ae3e4b53e09669349dc0e23da"));
                       owner
                       pool
                       liquidity
+                      tickLower
+                      tickUpper
                   }
               }
               `,
@@ -82,18 +83,20 @@ console.log(dsa_accounts.get("0x4dee144e4d60ad8ae3e4b53e09669349dc0e23da"));
     const datas1 = Object.values(res.data.data.positions);
 
     for (let data of datas1) {
-      
-      if (data.owner.toLowerCase() == addr_staker ) {
+      const poolContract = new ethers.Contract(data.pool, poolabi);
+      const slot0 = await poolContract.slot0();
+      const currentSqrtX96 = slot0[0];
+
+      if (data.owner.toLowerCase() == addr_staker) {
         let nft_owner = await staker_contract.deposits(data.id);
-          if(dsa_accounts.get(nft_owner.owner.toLowerCase()) == 1){
-            data.owner = nft_owner.owner.toLowerCase();
-            response.push(data);
-    }
-      }
-      else if (dsa_accounts.get(data.owner.toLowerCase()) == 1) {
+        if (dsa_accounts.get(nft_owner.owner.toLowerCase()) == 1) {
+          data.owner = nft_owner.owner.toLowerCase();
+          response.push(data);
+        }
+      } else if (dsa_accounts.get(data.owner.toLowerCase()) == 1) {
         response.push(data);
       }
-    }  
+    }
     thresh =
       res.data.data.positions[Object.values(res.data.data.positions).length - 1]
         .id;
@@ -101,5 +104,82 @@ console.log(dsa_accounts.get("0x4dee144e4d60ad8ae3e4b53e09669349dc0e23da"));
   }
   console.log("🚀 ~ file: script.js ~ line 73 ~ main ~ response", response);
 };
+
+function calculateSqrtPriceX96(tickLower, tickUpper) {
+  if (!lowerTick || lowerTick == 0) throw new Error("lowerTick not defined");
+  if (!upperTick || upperTick == 0) throw new Error("upperTick not defined");
+
+  const lowerPrice = 1.0001 ** Number(lowerTick);
+  const upperPrice = 1.0001 ** Number(upperTick);
+
+  const lowerSqrtPriceX36 = Math.round(Math.sqrt(lowerPrice) * 2 ** 36);
+  const upperSqrtPriceX36 = Math.round(Math.sqrt(upperPrice) * 2 ** 36);
+
+  const lowerSqrtPriceX96 = new BigNumber(
+    lowerSqrtPriceX36.toString()
+  ).multipliedBy(x60);
+  const upperSqrtPriceX96 = new BigNumber(
+    upperSqrtPriceX36.toString()
+  ).multipliedBy(x60);
+  return [lowerSqrtPriceX96, upperSqrtPriceX96];
+}
+
+async function _getAmounts(
+  liquidityContract,
+  currentSqrtPriceX96,
+  lowerTick,
+  upperTick,
+  liquidity
+) {
+  let [lowerSqrtPriceX96, upperSqrtPriceX96] = calculateSqrtPriceX96(
+    lowerTick,
+    upperTick
+  );
+
+  let { amount0, amount1 } = await liquidityContract.getAmountsForLiquidity(
+    currentSqrtPriceX96,
+    lowerSqrtPriceX96,
+    upperSqrtPriceX96,
+    liquidity
+  );
+
+  const tokenPrices = await axios.get(
+    "https://api.instadapp.io/defi/polygon/prices"
+  );
+
+  const price0 =
+    tokenPrices[
+      Object.keys(tokenPrices).find(
+        (a) => a.toLowerCase() === token0.toLowerCase()
+      )
+    ];
+  const price1 =
+    tokenPrices[
+      Object.keys(tokenPrices).find(
+        (a) => a.toLowerCase() === token1.toLowerCase()
+      )
+    ];
+
+  amount0Y = amount0Y
+    .multipliedBy(1e18)
+    .dividedBy(10 ** decimals0)
+    .multipliedBy(price0);
+  amount1Y = amount1Y
+    .multipliedBy(1e18)
+    .dividedBy(10 ** decimals1)
+    .multipliedBy(price1);
+
+  const token0USD = amount0
+    .multipliedBy(price0)
+    .dividedBy(10 ** decimals0)
+    .toString();
+  const token1USD = amount1
+    .multipliedBy(price1)
+    .dividedBy(10 ** decimals1)
+    .toString();
+
+  const totalAmountInUSD = BigNumber.sum(token0USD, token1USD);
+  return [new BigNumber(amount0.toString()), new BigNumber(amount1.toString())];
+}
 
 main();
